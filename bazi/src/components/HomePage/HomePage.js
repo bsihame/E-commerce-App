c292c6a675da5a0bb9d8a2ee8024e0a6039fd545
bsihame <sihamebazi@pursuit.org>
2021-02-26 10:39:18 -0500

    add first directory menue in the home page

--- bazi/src/components/HomePage/HomePage.js
+++ bazi/src/components/HomePage/HomePage.js
@@ -2,8 +2,16 @@ import React from "react";
 
 const HomePage = () => {
   return(
-    <div className="home_Page">
-    HELLO HOME Page
+    <div className="home-Page">
+      <div className="director-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Hats</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+   
     </div>
   )
 }
