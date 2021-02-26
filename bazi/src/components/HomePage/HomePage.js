fa2e464f81e52ee8135a50b1e3213625c9e7c57e
bsihame <sihamebazi@pursuit.org>
2021-02-26 10:43:38 -0500

    add more menue items  in the home page

--- bazi/src/components/HomePage/HomePage.js
+++ bazi/src/components/HomePage/HomePage.js
@@ -6,11 +6,39 @@ const HomePage = () => {
       <div className="director-menu">
         <div className="menu-item">
           <div className="content">
-            <h1 className="title">Hats</h1>
+            <h1 className="title">HATS</h1>
             <span className="subtitle">SHOP NOW</span>
           </div>
         </div>
       </div>
+
+      <div className="menu-item">
+          <div className="content">
+            <h1 className="title">JACKETS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+      </div>
+
+      <div className="menu-item">
+          <div className="content">
+            <h1 className="title">SHOES</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+      </div>
+
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">WOMEN</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">MEN</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
    
     </div>
   )
